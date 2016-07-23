f146c32115dca2677ee5d466270fe9d0039c60c9
gaoyve <gaoyve@outlook.com>
2016-07-23 23:38:02 +0800

    fix iOS 8 webView crash

--- ESPullToRefreshExample/ESPullToRefreshExample/WebViewController.swift
+++ ESPullToRefreshExample/ESPullToRefreshExample/WebViewController.swift
@@ -11,30 +11,42 @@ import UIKit
 class WebViewController: UIViewController, UIWebViewDelegate {
 
 
-    @IBOutlet weak var webView: UIWebView!
+    @IBOutlet weak var webViewXib: UIWebView!
+    var webView: UIWebView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.title = "https://github.com/eggswift"
-        self.webView?.delegate = self
-        if let webView = self.webView { // iOS 8 nil
-            print("\(webView)")         // iOS 9 not nil
+        
+        if let _ = self.webViewXib {
+            print("xib")
+            self.webView = self.webViewXib
+        } else {
+            print("code")
+            self.webView = UIWebView()
+            self.webView.frame = self.view.bounds
+            self.view.addSubview(self.webView!)
         }
         
-        self.webView?.scrollView.es_addPullToRefresh { [weak self] in
-            self?.webView!.loadRequest(NSURLRequest.init(URL: NSURL.init(string: "https://github.com/eggswift")!))
+        self.webView!.delegate = self
+        
+        let url = "https://github.com/eggswift"
+        self.title = url
+        let request = NSURLRequest(URL: NSURL(string: url)!)
+        
+        self.webView.scrollView.es_addPullToRefresh { [weak self] in
+            self!.webView.loadRequest(request)
         }
-        self.webView?.scrollView.es_startPullToRefresh()
+        self.webView.scrollView.es_startPullToRefresh()
     }
     
     func webViewDidFinishLoad(webView: UIWebView) {
-        self.webView?.scrollView.es_stopPullToRefresh(completion: true)
-        self.webView?.scrollView.bounces = true
-        self.webView?.scrollView.alwaysBounceVertical = true
+        self.webView.scrollView.es_stopPullToRefresh(completion: true)
+        self.webView.scrollView.bounces = true
+        self.webView.scrollView.alwaysBounceVertical = true
     }
     
     func webView(webView: UIWebView, didFailLoadWithError error: NSError?) {
-        self.webView?.scrollView.es_stopPullToRefresh(completion: false)
+        self.webView.scrollView.es_stopPullToRefresh(completion: false)
     }
 
 }
